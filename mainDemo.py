c706018ac5263b57125da82ae9bcae782f0cc1c7
Timourka <timourkot@gmail.com>
2026-04-27 22:20:43 +0400

    1

---
+++ mainDemo.py
@@ -0,0 +1,86 @@
+from robocad.common import CommonRobot
+import time
+import cv2
+
+robot = CommonRobot(False)
+
+
+def sleep(t):
+    time.sleep(t)
+
+
+def stop_motors():
+    robot.motor_speed_0 = 0
+    robot.motor_speed_1 = 0
+    robot.motor_speed_2 = 0
+    robot.motor_speed_3 = 0
+    robot.motor_speed_4 = 0
+    robot.motor_speed_5 = 0
+    sleep(0.3)
+
+
+def turn_right(seconds=1.0, speed=40):
+    robot.motor_speed_0 = speed
+    robot.motor_speed_1 = speed
+    robot.motor_speed_2 = speed
+    robot.motor_speed_3 = speed
+    robot.motor_speed_4 = speed
+    robot.motor_speed_5 = speed
+    sleep(seconds)
+    stop_motors()
+
+
+def turn_left(seconds=1.0, speed=40):
+    robot.motor_speed_0 = -speed
+    robot.motor_speed_1 = -speed
+    robot.motor_speed_2 = -speed
+    robot.motor_speed_3 = -speed
+    robot.motor_speed_4 = -speed
+    robot.motor_speed_5 = -speed
+    sleep(seconds)
+    stop_motors()
+
+
+def open_gripper():
+    robot.set_angle_servo(0, 2)
+    sleep(1)
+
+
+def close_gripper():
+    robot.set_angle_servo(90, 2)
+    sleep(1)
+
+
+def show_camera(seconds=10):
+    start = time.time()
+
+    while time.time() - start < seconds:
+        img = robot.camera_image
+
+        if img is not None:
+            cv2.imshow("Agrobot camera", img)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+        sleep(0.03)
+
+    cv2.destroyAllWindows()
+
+
+try:
+    for angle in [0, 30, 60, 90, 120, 150, 180]:
+        print("servo 2 angle:", angle)
+        robot.set_angle_servo(angle, 2)
+        time.sleep(1)
+    turn_right(1.0)
+    turn_left(1.0)
+
+    open_gripper()
+    close_gripper()
+
+    show_camera(100)
+    
+
+finally:
+    stop_motors()
