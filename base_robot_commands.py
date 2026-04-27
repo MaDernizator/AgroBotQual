6fdefa182945d95c1421b4a58dfdc00904ff345e
Timourka <timourkot@gmail.com>
2026-04-28 00:00:56 +0400

    base_movement

---
+++ base_robot_commands.py
@@ -0,0 +1,111 @@
+from robocad.common import CommonRobot
+import time
+import cv2
+
+def stop_motors(robot: CommonRobot):
+    robot.motor_speed_0 = 0
+    robot.motor_speed_1 = 0
+    robot.motor_speed_2 = 0
+    robot.motor_speed_3 = 0
+    robot.motor_speed_4 = 0
+    robot.motor_speed_5 = 0
+    
+def move_forward(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = speed
+    robot.motor_speed_1 = speed
+    robot.motor_speed_2 = speed
+    robot.motor_speed_3 = -speed
+    robot.motor_speed_4 = -speed
+    robot.motor_speed_5 = -speed
+
+
+def move_backward(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = -speed
+    robot.motor_speed_1 = -speed
+    robot.motor_speed_2 = -speed
+    robot.motor_speed_3 = speed
+    robot.motor_speed_4 = speed
+    robot.motor_speed_5 = speed
+
+
+def move_right(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = speed
+    robot.motor_speed_1 = speed
+    robot.motor_speed_2 = speed
+    robot.motor_speed_3 = speed
+    robot.motor_speed_4 = speed
+    robot.motor_speed_5 = speed
+
+
+def move_left(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = -speed
+    robot.motor_speed_1 = -speed
+    robot.motor_speed_2 = -speed
+    robot.motor_speed_3 = -speed
+    robot.motor_speed_4 = -speed
+    robot.motor_speed_5 = -speed
+
+
+def move_forward_right(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = speed
+    robot.motor_speed_1 = speed
+    robot.motor_speed_2 = speed
+    robot.motor_speed_3 = -speed // 2
+    robot.motor_speed_4 = -speed // 2
+    robot.motor_speed_5 = -speed // 2
+
+
+def move_forward_left(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = speed // 2
+    robot.motor_speed_1 = speed // 2
+    robot.motor_speed_2 = speed // 2
+    robot.motor_speed_3 = -speed
+    robot.motor_speed_4 = -speed
+    robot.motor_speed_5 = -speed
+
+
+def move_backward_right(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = -speed
+    robot.motor_speed_1 = -speed
+    robot.motor_speed_2 = -speed
+    robot.motor_speed_3 = speed // 2
+    robot.motor_speed_4 = speed // 2
+    robot.motor_speed_5 = speed // 2
+
+
+def move_backward_left(robot: CommonRobot, speed=50):
+    robot.motor_speed_0 = -speed // 2
+    robot.motor_speed_1 = -speed // 2
+    robot.motor_speed_2 = -speed // 2
+    robot.motor_speed_3 = speed
+    robot.motor_speed_4 = speed
+    robot.motor_speed_5 = speed
+
+def open_gripper(robot : CommonRobot, ):
+    robot.set_angle_servo(300, 2)
+
+def close_gripper(robot : CommonRobot, ):
+    robot.set_angle_servo(0, 2)
+    
+def lift_up__gripper(robot : CommonRobot, ):
+    robot.set_angle_servo(300, 1)
+    
+def lift_down_gripper(robot : CommonRobot, ):
+    robot.set_angle_servo(30, 1)
+
+
+def show_camera(robot : CommonRobot, seconds=10):
+    start = time.time()
+
+    while time.time() - start < seconds:
+        img = robot.camera_image
+
+        if img is not None:
+            cv2.imshow("Agrobot camera", img)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+        time.sleep(0.03)
+
+    cv2.destroyAllWindows()
--- mainDemo.py
+++ mainDemo.py
@@ -1,71 +1,30 @@
 from robocad.common import CommonRobot
 import time
-import cv2
+
+import base_robot_commands as cmds
 
 robot = CommonRobot(False)
 
-def stop_motors():
-    robot.motor_speed_0 = 0
-    robot.motor_speed_1 = 0
-    robot.motor_speed_2 = 0
-    robot.motor_speed_3 = 0
-    robot.motor_speed_4 = 0
-    robot.motor_speed_5 = 0
-
-
-def turn_right(speed=40):
-    robot.motor_speed_0 = speed
-    robot.motor_speed_1 = speed
-    robot.motor_speed_2 = speed
-    robot.motor_speed_3 = speed
-    robot.motor_speed_4 = speed
-    robot.motor_speed_5 = speed
-
-
-def turn_left(speed=40):
-    robot.motor_speed_0 = -speed
-    robot.motor_speed_1 = -speed
-    robot.motor_speed_2 = -speed
-    robot.motor_speed_3 = -speed
-    robot.motor_speed_4 = -speed
-    robot.motor_speed_5 = -speed
-
-
-def open_gripper():
-    robot.set_angle_servo(300, 2)
-    sleep(1)
-
-
-def close_gripper():
-    robot.set_angle_servo(0, 2)
+def main():
+    cmds.lift_up__gripper(robot)
     
-def lift_up__gripper():
-    robot.set_angle_servo(300, 1)
+    # cmds.move_forward_right(robot, 50)
+    # time.sleep(1)
+    # cmds.stop_motors(robot)
     
-def lift_down_gripper():
-    robot.set_angle_servo(30, 1)
+    # cmds.lift_up__gripper(robot)
+    # time.sleep(1)
 
-
-def show_camera(seconds=10):
-    start = time.time()
-
-    while time.time() - start < seconds:
-        img = robot.camera_image
-
-        if img is not None:
-            cv2.imshow("Agrobot camera", img)
-
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-        time.sleep(0.03)
-
-    cv2.destroyAllWindows()
-
-
+    # cmds.move_forward(robot, 50)
+    # time.sleep(1)
+    # cmds.move_forward_right(robot, 50)
+    # time.sleep(1)
+    # cmds.move_right(robot, 50)
+    # time.sleep(1)
+    # cmds.stop_motors(robot)
+        
 try:
-    show_camera(100)
-    
-
+    main()
 finally:
-    stop_motors()
+    cmds.stop_motors(robot)
+    robot.stop()
